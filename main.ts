fb289693679fddb16a193a8f2fbadd18bf56a711
J <112033133+trulyjustj@users.noreply.github.com>
2024-01-18 16:06:25 -0600

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -9,13 +9,18 @@ scene.onHitWall(SpriteKind.Player, function (sprite, location) {
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (jump < 2) {
         jump += 1
-        boy.vy = -150
+        mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).vy = -150
+        mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).vy = -150
     }
 })
 let jump = 0
-let boy: Sprite = null
 scene.setBackgroundColor(13)
-boy = sprites.create(assets.image`myImage1`, SpriteKind.Player)
-controller.moveSprite(boy, 100, 0)
-boy.ay = 300
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`myImage1`, SpriteKind.Player))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`myImage0`, SpriteKind.Player))
+controller.player1.moveSprite(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)), 100, 0)
+controller.player2.moveSprite(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)), 100, 0)
+mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).ay = 300
+mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).ay = 300
 jump = 0
+tiles.setCurrentTilemap(tilemap`level2`)
+scene.cameraFollowSprite(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)))
