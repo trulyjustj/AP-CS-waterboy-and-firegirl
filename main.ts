021034a8cad71b4b435201d8b5dc6cd933a55c85
J <112033133+trulyjustj@users.noreply.github.com>
2024-01-19 15:19:41 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,16 @@
-scene.onHitWall(SpriteKind.Player, function (sprite, location) {
-    if (!(sprite.isHittingTile(CollisionDirection.Top))) {
-        jump = 0
-    }
-    if (sprite.isHittingTile(CollisionDirection.Right) || sprite.isHittingTile(CollisionDirection.Left)) {
-        sprite.vy = 0
-    }
-})
+namespace SpriteKind {
+    export const boy = SpriteKind.create()
+    export const girl = SpriteKind.create()
+}
 controller.player1.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
     if (jump < 2) {
         jump += 1
         mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).vy = -150
     }
     pause(1000)
+})
+scene.onOverlapTile(SpriteKind.girl, sprites.dungeon.hazardWater, function (sprite, location) {
+    game.gameOver(false)
 })
 controller.player2.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
     if (jump < 2) {
@@ -20,12 +19,31 @@ controller.player2.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pres
     }
     pause(1000)
 })
+scene.onHitWall(SpriteKind.girl, function (sprite, location) {
+    if (!(sprite.isHittingTile(CollisionDirection.Top))) {
+        jump = 0
+    }
+    if (sprite.isHittingTile(CollisionDirection.Right) || sprite.isHittingTile(CollisionDirection.Left)) {
+        sprite.vy = 0
+    }
+})
+scene.onOverlapTile(SpriteKind.boy, sprites.dungeon.hazardLava0, function (sprite, location) {
+    game.gameOver(false)
+})
+scene.onHitWall(SpriteKind.boy, function (sprite, location) {
+    if (!(sprite.isHittingTile(CollisionDirection.Top))) {
+        jump = 0
+    }
+    if (sprite.isHittingTile(CollisionDirection.Right) || sprite.isHittingTile(CollisionDirection.Left)) {
+        sprite.vy = 0
+    }
+})
 let jump = 0
 scene.setBackgroundColor(13)
 tiles.setCurrentTilemap(tilemap`level2`)
-mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`myImage1`, SpriteKind.Player))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`myImage1`, SpriteKind.boy))
 tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)), tiles.getTileLocation(2, 19))
-mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`myImage0`, SpriteKind.Player))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`myImage0`, SpriteKind.girl))
 tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)), tiles.getTileLocation(2, 22))
 scene.cameraFollowSprite(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)))
 controller.player1.moveSprite(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)), 100, 0)
